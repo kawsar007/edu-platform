391117d51081063a450bbdd8f71ff250f3c03c05
kawsar007 <imkawsar007@gmail.com>
2024-09-07 00:45:52 +0600

    DB issue fix

--- queries/courses.js
+++ queries/courses.js
@@ -1,5 +1,7 @@
 import { Category } from "@/model/category-model";
 import { Course } from "@/model/course-model";
+import { Module } from "@/model/module.model";
+import { Testimonial } from "@/model/testimonial-model";
 import { User } from "@/model/user-model";
 
 export async function getCourses() {
@@ -9,6 +11,12 @@ export async function getCourses() {
   }).populate({
     path: "instructor",
     model: User
+  }).populate({
+    path: "testimonials",
+    model: Testimonial
+  }).populate({
+    path: "modules",
+    model: Module
   });
   return courses;
 }
