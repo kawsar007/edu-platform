363543df445b815583d271643df2bd8b057eb19f
kawsar007 <imkawsar007@gmail.com>
2024-10-18 18:06:58 +0600

    Implemented OAuth

---
+++ app/login/_components/social-login.jsx
@@ -0,0 +1,28 @@
+import { doSocialLogin } from "@/app/actions";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+
+const SocialLogin = () => {
+  return (
+    <>
+      <div className='text-center text-md mt-3 text-gray-500'>
+        or Signup with
+      </div>
+      <form action={doSocialLogin}>
+        <div className='flex justify-center gap-2'>
+          <Button
+            className='mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center'
+            type='submit'
+            name='action'
+            value='google'>
+            <Image src='/google.png' alt='google' width={40} height={40} />
+            <span>Google</span>
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default SocialLogin;
--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 import { User } from "@/model/user-model";
 
@@ -39,5 +40,16 @@ export const {
         }
       }
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }) ,
   ],
 });
