a92b8ad0a36b09ddb9d9c04c253261cef5efc6ce
kawsar007 <imkawsar007@gmail.com>
2024-11-02 20:54:03 +0600

    Design Assessment and Report Model

---
+++ model/report-model.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+const reportSchema = new Schema({
+  totalCompletedLessons: {
+    required: true,
+    typr: Array,
+  },
+  totalCompletedModeules: {
+    required: true,
+    type: Array,
+  },
+
+  course: { type: Schema.ObjectId, ref: "Course" },
+  student: { type: Schema.ObjectId, ref: "User" },
+  quizAssessment: { type: Schema.ObjectId, ref: "Assessment" }
+});
+
+export const Report = mongoose.models.Report ?? mongoose.model("Report", reportSchema);
