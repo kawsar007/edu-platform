0ba676e632b8d8e700eda67722fe926784d0eccb
kawsar007 <imkawsar007@gmail.com>
2025-01-21 16:44:16 +0600

    Fetch list of quizzes from DB and render in UI

--- app/dashboard/quiz-sets/[quizSetId]/page.jsx
+++ app/dashboard/quiz-sets/[quizSetId]/page.jsx
@@ -1,18 +1,11 @@
-"use client";
 import AlertBanner from "@/components/alert-banner";
-import { IconBadge } from "@/components/icon-badge";
-import { LayoutDashboard } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { getQuizSetById } from "@/queries/quizzes";
+import { Circle, CircleCheck, Pencil, Trash } from "lucide-react";
+import { AddQuizForm } from "./_components/add-quiz-form";
 import { QuizSetAction } from "./_components/quiz-set-action";
 import { TitleForm } from "./_components/title-form";
-import { AddQuizForm } from "./_components/add-quiz-form";
-import { cn } from "@/lib/utils";
-import { useState } from "react";
-import { Pencil } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Delete } from "lucide-react";
-import { Trash } from "lucide-react";
-import { CircleCheck } from "lucide-react";
-import { Circle } from "lucide-react";
 const initialQuizes = [
   {
     id: 1,
@@ -59,49 +52,65 @@ const initialQuizes = [
     ],
   },
 ];
-const EditQuizSet = () => {
-  const [quizes, setQuizes] = useState(initialQuizes);
+const EditQuizSet = async ({ params: { quizSetId } }) => {
+  const quizSet = await getQuizSetById(quizSetId);
+  
+  const quizzes = quizSet.quizIds.map((quiz) => {
+    return {
+      id: quiz._id.toString(),
+      title: quiz.title,
+      options: quiz.options.map((option) => {
+        return {
+          label: option.text,
+          isTrue: option.is_correct,
+        };
+      }),
+    };
+  });
+  console.log("Quiz Set quizes ---> ", quizzes);
+
   return (
     <>
       <AlertBanner
-        label="This course is unpublished. It will not be visible in the course."
-        variant="warning"
+        label='This course is unpublished. It will not be visible in the course.'
+        variant='warning'
       />
-      <div className="p-6">
-        <div className="flex items-center justify-end">
+      <div className='p-6'>
+        <div className='flex items-center justify-end'>
           <QuizSetAction />
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2  gap-6 mt-16">
+        <div className='grid grid-cols-1 lg:grid-cols-2  gap-6 mt-16'>
           {/* Quiz List */}
-          <div className="max-lg:order-2">
-            <h2 className="text-xl mb-6">Quiz List</h2>
-            <AlertBanner
-              label="No Quiz are in the set, add some using the form above."
-              variant="warning"
-              className="rounded mb-6"
-            />
-            <div className="space-y-6">
-              {quizes.map((quiz) => {
+          <div className='max-lg:order-2'>
+            <h2 className='text-xl mb-6'>Quiz List</h2>
+            {quizzes.length === 0 && (
+              <AlertBanner
+                label='No Quiz are in the set, add some using the form above.'
+                variant='warning'
+                className='rounded mb-6'
+              />
+            )}
+
+            <div className='space-y-6'>
+              {quizzes.map((quiz) => {
                 return (
                   <div
                     key={quiz.id}
-                    className=" bg-gray-50 shadow-md p-4 lg:p-6 rounded-md border"
-                  >
-                    <h2 className="mb-3">{quiz.title}</h2>
+                    className=' bg-gray-50 shadow-md p-4 lg:p-6 rounded-md border'>
+                    <h2 className='mb-3'>{quiz.title}</h2>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+                    <div className='grid grid-cols-1 sm:grid-cols-2 gap-3'>
                       {quiz.options.map((option) => {
                         return (
                           <div
                             className={cn(
-                              "py-1.5 rounded-sm  text-sm flex items-center gap-1 text-gray-600"
+                              "py-1.5 rounded-sm  text-sm flex items-center gap-1 text-gray-600",
                             )}
-                            key={option.label}
-                          >
+                            key={option.label}>
                             {option.isTrue ? (
-                              <CircleCheck className="size-4 text-emerald-500 " />
+                              <CircleCheck className='size-4 text-emerald-500 ' />
                             ) : (
-                              <Circle className="size-4" />
+                              <Circle className='size-4' />
                             )}
 
                             <p>{option.label}</p>
@@ -109,16 +118,15 @@ const EditQuizSet = () => {
                         );
                       })}
                     </div>
-                    <div className="flex items-center justify-end gap-2 mt-6">
-                      <Button variant="ghost" size="sm">
-                        <Pencil className="w-3 mr-1" /> Edit
+                    <div className='flex items-center justify-end gap-2 mt-6'>
+                      <Button variant='ghost' size='sm'>
+                        <Pencil className='w-3 mr-1' /> Edit
                       </Button>
                       <Button
-                        size="sm"
-                        className="text-destructive"
-                        variant="ghost"
-                      >
-                        <Trash className="w-3 mr-1" /> Delete
+                        size='sm'
+                        className='text-destructive'
+                        variant='ghost'>
+                        <Trash className='w-3 mr-1' /> Delete
                       </Button>
                     </div>
                   </div>
@@ -128,10 +136,10 @@ const EditQuizSet = () => {
           </div>
           {/*  */}
           <div>
-            <div className="flex items-center gap-x-2">
-              <h2 className="text-xl">Customize your quiz set</h2>
+            <div className='flex items-center gap-x-2'>
+              <h2 className='text-xl'>Customize your quiz set</h2>
             </div>
-            <div className="max-w-[800px]">
+            <div className='max-w-[800px]'>
               <TitleForm
                 initialData={{
                   title: "Reactive Accelerator",
@@ -139,8 +147,9 @@ const EditQuizSet = () => {
               />
             </div>
 
-            <div className="max-w-[800px]">
-              <AddQuizForm setQuizes={setQuizes} />
+            <div className='max-w-[800px]'>
+              {/* <AddQuizForm setQuizes={setQuizes} /> */}
+              <AddQuizForm />
             </div>
           </div>
         </div>
--- queries/quizzes.js
+++ queries/quizzes.js
@@ -1,16 +1,25 @@
-import { replaceMongoIdInArray } from "@/lib/convertData";
+import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData";
 import { Quizset } from "@/model/quizset-model";
 import { Quiz } from "@/model/quizzes-model";
 
 
 export async function getAllQuizSets() {
   try {
-    const quizSets = await Quizset.find().populate({
-      path: "quizIds",
-      model: Quiz
-    }).lean();
+    const quizSets = await Quizset.find().lean();
     return replaceMongoIdInArray(quizSets);
   } catch (e) {
     throw new Error(e);
+  }
+}
+
+export async function getQuizSetById(id) {
+  try {
+    const quizSet = await Quizset.findById(id).populate({
+      path: "quizIds",
+      model: Quiz
+    }).lean();
+    return replaceMongoIdInObject(quizSet);
+  } catch (error) {
+    throw new Error(error);
   }
 }
